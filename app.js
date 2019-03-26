20f8936f87bb845fe635d77c261b79a2aaa2dfbc
saud9030 <saud.amakn@gmail.com>
2019-03-26 08:15:32 +0300

    installed bodyParser, added the base of group route(that are needed now)

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import group from "./routes/group_route";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const port = process.env.PORT || 4000;
-
+app.use(bodyParser.json());
 app.use(group);
 // checking that is working
 app.get("/", (req, res) => {
--- routes/group_route.js
+++ routes/group_route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import models from "../db/models";
+import bodyParser from "body-parser";
 
 const route = express();
 
@@ -8,6 +9,46 @@ route.get("/api/group", (req, res) => {
   models.Group.findAll()
     .then(group => {
       res.status(200).json({ group: group });
+    })
+    .catch(e => console.log(e));
+});
+
+//to get the information of a specific voulnteer group **need to add authentication
+route.get("/api/group/:id", (req, res) => {
+  models.Group.findByPk(req.params.id)
+    .then(group => {
+      res.status(200).json({ group });
+    })
+    .catch((e = console.log(e)));
+});
+
+//to create new volunteer group ** need to authenticate and check for any duplicates.
+route.post("/api/group", (req, res) => {
+  models.Group.create(req.body)
+    .then(group => {
+      res.status(200).json({ group });
+    })
+    .catch(e => console.log(e));
+});
+
+//to update group info, or it's members ** need authorization to specific member
+route.put("/api/group/:id", (req, res) => {
+  models.Group.findByPk(req.params.id)
+    .then(group => {
+      group
+        .update({
+          name: req.body.name,
+          city: req.body.city,
+          founded: req.body.founded,
+          description: req.body.description,
+          contactNumber: req.body.contactNumber,
+          Email: req.body.email,
+          type: req.body.type
+        })
+        .then(group => {
+          res.status(200).json({ group });
+        })
+        .catch(e => console.log(e));
     })
     .catch(e => console.log(e));
 });
