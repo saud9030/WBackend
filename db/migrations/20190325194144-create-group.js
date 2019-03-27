f6449f84bfbfb8a5c8ec3f12cd96cf3e2c8002f1
saud9030 <saud.amakn@gmail.com>
2019-03-27 06:31:51 +0300

    undid all migrations, and added validations into Group and User migration files

--- db/migrations/20190325194144-create-group.js
+++ db/migrations/20190325194144-create-group.js
@@ -9,28 +9,49 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       city: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       leader: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       founded: {
-        type: Sequelize.DATEONLY
+        type: Sequelize.DATEONLY,
+        allowNull: false
       },
       description: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
+        allowNull: false
       },
       contactNumber: {
-        type: Sequelize.DOUBLE
+        type: Sequelize.DOUBLE,
+        allowNull: true,
+        validate: {
+          isNumeric: {
+            args: true,
+            msg: "Only numbers are acceptable"
+          },
+          // to ensure that the user will add 10 digits whenever they decide to add a number.
+          givenNumber(num) {
+            if (num === null && num.length !== 10) {
+              throw new error(`your contact number should have 10 digits`);
+            }
+          }
+        }
       },
       Email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        isEmail: true
       },
       type: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       createdAt: {
         allowNull: false,
--- db/migrations/20190325195713-create-user.js
+++ db/migrations/20190325195713-create-user.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
+    return queryInterface.createTable("Users", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -9,21 +9,53 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        isUnique: {
+          args: true,
+          msg: "name is already taken"
+        },
+        validate: {
+          len: {
+            args: [3, 20],
+            msg: "your name should be between 3 and 20 letters"
+          }
+        }
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          isEmail: true
+        },
+        isUnique: {
+          args: true,
+          msg: "email is already taken"
+        }
       },
       password: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       age: {
-        type: Sequelize.INTEGER
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        validate: {
+          // to limit the user's age between 18 and 80
+          allowedAge(age) {
+            if (age < 18 || age > 80) {
+              throw new error(`you're probably too young to sign up`);
+            }
+          }
+        }
       },
       gender: {
+        allowNull: false,
+        // true is for male, and false is for female
         type: Sequelize.BOOLEAN
       },
       occupation: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       bio: {
@@ -40,6 +72,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
+    return queryInterface.dropTable("Users");
   }
 };
