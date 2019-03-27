19b04c58ca6c376c04bda93a719e0cbb9987f0db
saud9030 <saud.amakn@gmail.com>
2019-03-27 07:03:41 +0300

    correct a wrong spelling in volunteeringevent migration file, and added validations

--- db/migrations/20190325200637-create-volunteeringevent.js
+++ db/migrations/20190325200637-create-volunteeringevent.js
@@ -9,13 +9,34 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          len: {
+            args: [5,50],
+            msg: "the title is too long, add more details in the info section"
+          },
+      },
+      info: {
+        type: Sequelize.TEXT,
+        allowNull: false,
       },
       address: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
-      data: {
-        type: Sequelize.DATEONLY
+      date: {
+        type: Sequelize.DATEONLY,
+        validate: {
+          // to ensure that the user have chosen the right date but limiting the date to today and after
+          checkDate(day) {
+            let today = new Date();
+            let validDate = today.setDate(today.getDate()-1);
+            if (day < validDate){
+              throw new error('you only can choose from today and onward')
+            }
+          }
+        }
       },
       type: {
         type: Sequelize.STRING
