764a29820bbe480e7fdc5d837d08a9a3a791666d
saud9030 <saud.amakn@gmail.com>
2019-03-27 09:13:23 +0300

    fixed bug in the volunteeringevent migration file

--- db/migrations/20190325200637-create-volunteeringevent.js
+++ db/migrations/20190325200637-create-volunteeringevent.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Volunteeringevents', {
+    return queryInterface.createTable("Volunteeringevents", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -13,13 +13,14 @@ module.exports = {
         allowNull: false,
         validate: {
           len: {
-            args: [5,50],
+            args: [5, 50],
             msg: "the title is too long, add more details in the info section"
-          },
+          }
+        }
       },
       info: {
         type: Sequelize.TEXT,
-        allowNull: false,
+        allowNull: false
       },
       address: {
         type: Sequelize.STRING,
@@ -31,9 +32,9 @@ module.exports = {
           // to ensure that the user have chosen the right date but limiting the date to today and after
           checkDate(day) {
             let today = new Date();
-            let validDate = today.setDate(today.getDate()-1);
-            if (day < validDate){
-              throw new error('you only can choose from today and onward')
+            let validDate = today.setDate(today.getDate() - 1);
+            if (day < validDate) {
+              throw new error("you only can choose from today and onward");
             }
           }
         }
@@ -52,6 +53,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Volunteeringevents');
+    return queryInterface.dropTable("Volunteeringevents");
   }
 };
