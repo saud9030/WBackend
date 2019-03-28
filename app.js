f077cf02db46241f5a823389812e35b5f64a40a9
saud9030 <saud.amakn@gmail.com>
2019-03-28 14:25:36 +0300

    added files to lib folder such as error_handler, and edit the last two lines in the index.js model

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// loads environment variables from a .env file into process.env
+import dotenv from "dotenv";
+dotenv.config();
+
 import models from "./db/models";
 import group from "./routes/group_route";
 import user from "./routes/user_route";
@@ -5,8 +9,14 @@ import bodyParser from "body-parser";
 import passport from "passport";
 import strategy from "./lib/passportStrategy";
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+// set CORS headers on response from this API using the `cors` NPM package
+// `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
+app.use(cors({ origin: process.env.CLIENT_ORIGIN, credentials: true }));
 
 const port = process.env.PORT || 4000;
 
@@ -16,6 +26,8 @@ passport.use(strategy);
 //** middleware **/
 app.use(passport.initialize());
 app.use(bodyParser.json());
+// Parse Cookie header and populate req.cookies
+app.use(cookieParser());
 app.use(group);
 app.use(user);
 
